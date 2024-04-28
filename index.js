2ad96c7a9176eb80e6843c29a7c0919767184b72
Chetan717 <chaitanyachaudhari717@gmail.com>
2024-04-28 16:56:36 +0530

    sdf

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const helmet = require("helmet");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const Razorpay = require("razorpay")
+
 dotenv.config({ path: "./.env" });
+app.use(express.json());
+
 app.use(helmet());
 app.use(
   cors({
@@ -22,6 +26,10 @@ app.use(
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+const razorpay = new Razorpay({
+  key_id: "rzp_test_AwYjl9iEgMP9Zk",
+  key_secret: "ZU6SUT3OJbRroetooMuIT6TC"
+})
 const apolloServerStarter = async () => {
   try {
     const Server = new ApolloServer({
@@ -35,6 +43,32 @@ const apolloServerStarter = async () => {
       useNewUrlParser: true,
     });
     console.log("database Connected....");
+
+    app.post("/crorder", async (req, res) => {
+      try {
+        const { amount } = req.body;
+        const currentDate = new Date();
+
+        const Order = await razorpay.orders.create({
+          amount: Number(amount * 100),
+          currency: "INR",
+          receipt: `${currentDate.getSeconds()}`
+
+        });
+        res.status(200).json({
+          success: true,
+          order_id: Order.id,
+          amount: Number(amount * 100),
+        });
+      } catch (error) {
+        // console.error("Error creating order:", error);
+        res.status(500).json({
+          success: false,
+          error: "Error creating order"
+        });
+      }
+    });
+
     app.get("/", (req, res) => {
       res.send("we are live!");
     });
